59af3fc1d30c5d83f600498c8219bf0bb356236d
Sveinar Søpler <cybermax@dexter.no>
2024-03-13 16:53:08 +0100

    Update proton script
    Make the script slightly lighter, and should make it work when using
    Proton9 (Tested with GE-Proton-9.1).

--- proton_setup.sh
+++ proton_setup.sh
@@ -20,61 +20,20 @@ if [ ! -f "$PROTON_LIBS/files/$lib/$arch-windows/dxgi.dll" ]; then
     exit 1
 fi
 
-function fake_install {
-    cp -f "$nvlibs_dir/$lib/$arch-windows/$1" "$PROTON_LIBS/files/$lib/$arch-windows/"
-    chmod -w,+x "$PROTON_LIBS/files/$lib/$arch-windows/$1"
-}
-
-function install {
-    cp -f "$nvlibs_dir/$lib/$arch-unix/$1" "$PROTON_LIBS/files/$lib/$arch-unix/"
-    chmod -w,+x "$PROTON_LIBS/files/$lib/$arch-unix/$1"
-}
-
-function nvapi {
-     cp -f "$nvlibs_dir/$lib/$arch-windows/$1" "$PROTON_LIBS/files/$lib/nvapi/"
-     chmod -w,+x "$PROTON_LIBS/files/$lib/nvapi/$1"
-}
-
-function default_pfx {
-     if [ "$arch" == "i386" ]; then
-         cd "$PROTON_LIBS/files/share/default_pfx/drive_c/windows/syswow64"
-         ln -sf "../../../../../lib/wine/$arch-windows/$1" "$1"
-     else
-         cd "$PROTON_LIBS/files/share/default_pfx/drive_c/windows/system32"
-         ln -sf "../../../../../lib64/wine/$arch-windows/$1" "$1"
-     fi
-}
-
-fun=fake_install
-$fun nvcuda.dll
-
-fun=install
-$fun nvcuda.dll.so
-
-fun=nvapi
-$fun nvapi.dll
-
-fun=default_pfx
-$fun nvcuda.dll
+cp -f "$nvlibs_dir/$lib/$arch-unix/nvcuda.dll.so" "$PROTON_LIBS/files/$lib/$arch-windows/nvcuda.dll"
+cp -f "$nvlibs_dir/$lib/$arch-windows/nvapi.dll" "$PROTON_LIBS/files/$lib/nvapi"
 
 arch='x86_64'
 lib='lib64/wine'
-fun=fake_install
-$fun nvcuda.dll
-$fun nvml.dll
-$fun nvoptix.dll
 
-fun=install
-$fun nvcuda.dll.so
-$fun nvml.so
-$fun nvoptix.dll.so
-
-fun=nvapi
-$fun nvapi64.dll
-
-fun=default_pfx
-$fun nvcuda.dll
-$fun nvml.dll
-$fun nvoptix.dll
+cp -f "$nvlibs_dir/$lib/$arch-unix/nvcuda.dll.so" "$PROTON_LIBS/files/$lib/$arch-windows/nvcuda.dll"
+cp -f "$nvlibs_dir/$lib/$arch-unix/nvoptix.dll.so" "$PROTON_LIBS/files/$lib/$arch-windows/nvoptix.dll"
+cp -f "$nvlibs_dir/$lib/$arch-unix/nvml.so" "$PROTON_LIBS/files/$lib/$arch-unix"
+cp -f "$nvlibs_dir/$lib/$arch-windows/nvml.dll" "$PROTON_LIBS/files/$lib/$arch-windows"
+cp -f "$nvlibs_dir/$lib/$arch-windows/nvapi64.dll" "$PROTON_LIBS/files/$lib/nvapi"
+cd "$PROTON_LIBS/files/share/default_pfx/drive_c/windows/system32"
+ln -sf "../../../../../lib64/wine/$arch-windows/nvml.dll" "nvml.dll"
+ln -sf "../../../../../lib64/wine/$arch-windows/nvoptix.dll" "nvoptix.dll"
+cd "$nvlibs_dir"
 
 echo -ne "All done - Files dropped in $PROTON_LIBS\n"
